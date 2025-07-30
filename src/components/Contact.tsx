9d7c5235cc66052b376f56162e5f91a15467ed90
Ebube Ezedimbu <eezedimbu@gmail.com>
2025-07-30 18:28:27 +0100

    blank

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,19 +20,19 @@ export default function Contact() {
         </div>
       </figure>
 
-      <form className='md:flex-1 w-screen pl-8 pr-25 md:pr-10'>
+      <form className='md:flex-1 w-screen pl-8 pr-10 md:pr-10'>
         <img src={keyboard} alt="keyboard" className="w-[25%] mb-15 ml-10" />
 
         <section className="flex flex-col md:flex-row md:items-center gap-3 mb-3">
 
           <div>
             <label htmlFor="name" className="block mb-2">Your Name</label>
-            <input type="text" name="name" placeholder='Name' className="bg-[#393E4680] px-5 py-3 rounded-2xl font-bold"/>
+            <input type="text" name="name" placeholder='Name' className="bg-[#393E4680] px-5 py-3 rounded-2xl font-bold w-full"/>
           </div>
 
           <div>
             <label htmlFor="email" className="block mb-3">Your Email</label>
-            <input type="email" name="email" placeholder='Email' className='bg-[#393E4680] px-5 py-3 rounded-2xl font-bold' />
+            <input type="email" name="email" placeholder='Email' className='bg-[#393E4680] px-5 py-3 rounded-2xl font-bold w-full' />
           </div>
 
         </section>
@@ -42,7 +42,7 @@ export default function Contact() {
         <label htmlFor="message" className='block mb-3'>Your Message</label>
         <textarea name="message" placeholder="Message" className='bg-[#393E4680] rounded-2xl p-3 mb-5 font-bold w-full h-40 ' ></textarea>
 
-        <Button variant="primary" type="submit" className="flex justify-center w-full">Send Message <FaTelegramPlane className="inline"/></Button>
+        <Button variant="primary" type="submit" className="flex justify-center items-center gap-3 w-full">Send Message <FaTelegramPlane className="inline"/></Button>
 
         <img src={mail} alt="mail" className="transform translate-x-90 mt-5 w-[10%]" />
 
